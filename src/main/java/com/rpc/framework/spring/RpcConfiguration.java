e80c06a8dabc08cf5ee01353c4b058e23906bfc9
liujiawei <hzliujiawei@corp.netease.com>
2017-04-22 12:43:40 +0800

    no message

---
+++ src/main/java/com/rpc/framework/spring/RpcConfiguration.java
@@ -0,0 +1,10 @@
+package com.rpc.framework.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by liujiawei on 2017/4/22.
+ */
+@Configuration
+public class RpcConfiguration {
+}
