f7f82558d3a2e858e4191780a88037cc480566e5
liujiawei <hzliujiawei@corp.netease.com>
2017-04-22 10:06:41 +0800

    创建工程

---
+++ src/main/java/com/rpc/framework/core/ClassA.java
@@ -0,0 +1,7 @@
+package com.rpc.framework.core;
+
+/**
+ * Created by liujiawei on 2017/4/22.
+ */
+public class ClassA {
+}
---
+++ src/main/java/com/rpc/framework/model/ModelA.java
@@ -0,0 +1,7 @@
+package com.rpc.framework.model;
+
+/**
+ * Created by liujiawei on 2017/4/22.
+ */
+public class ModelA {
+}
